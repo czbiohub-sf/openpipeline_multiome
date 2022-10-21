3f640e0c5d768605c04c758577da68f9020a44e6
yangjun9095 <yangjun9095@gmail.com>
2022-10-21 18:34:01 +0000

    added cellranger_arc_count component

---
+++ src/mapping/cellranger_arc_count/script.sh
@@ -0,0 +1,84 @@
+#!/bin/bash
+
+set -eo pipefail
+
+## VIASH START
+par_input='resources_test/cellranger_arc_tiny_fastqs/cellranger_arc_libraries.csv'
+par_reference='resources_test/cellranger_arc_tiny_fastqs/cellranger_arc_tiny_ref/'
+par_output='resources_test/cellranger_arc_tiny_fastqs/bam'
+par_expect_cells="3000"
+par_secondary_analysis="false"
+## VIASH END
+
+# just to make sure paths are absolute
+par_reference=`realpath $par_reference`
+par_output=`realpath $par_output`
+
+# create temporary directory
+tmpdir=$(mktemp -d "$VIASH_TEMP/$meta_functionality_name-XXXXXXXX")
+function clean_up {
+    rm -rf "$tmpdir"
+}
+trap clean_up EXIT
+
+# process inputs
+# for every fastq file found, make a symlink into the tempdir
+fastq_dir="$tmpdir/fastqs"
+mkdir -p "$fastq_dir"
+IFS=";"
+for var in $par_input; do
+  unset IFS
+  abs_path=`realpath $var`
+  if [ -d "$abs_path" ]; then
+    find "$abs_path" -name *.fastq.gz -exec ln -s {} "$fastq_dir" \;
+  else
+    ln -s "$abs_path" "$fastq_dir"
+  fi
+done
+
+# cd into tempdir
+cd "$tmpdir"
+
+# add additional params
+extra_params=( )
+
+if [ ! -z "$meta_cpus" ]; then 
+  extra_params+=( "--localcores=$meta_cpus" )
+fi
+if [ ! -z "$meta_memory_gb" ]; then 
+  # always keep 2gb for the OS itself
+  memory_gb=`python -c "print(int('$meta_memory_gb') - 2)"`
+  extra_params+=( "--localmem=$memory_gb" )
+fi
+if [ ! -z "$par_expect_cells" ]; then 
+  extra_params+=( "--expect-cells=$par_expect_cells" )
+fi
+if [ ! -z "$par_chemistry" ]; then 
+  extra_params+=( "--chemistry=$par_chemistry" )
+fi
+if [ "$par_secondary_analysis" == "false" ]; then
+  extra_params+=( "--nosecondary" )
+fi
+if [ "$par_generate_bam" == "false" ]; then
+  extra_params+=( "--no-bam" )
+fi
+echo "Running cellranger_arc count"
+
+
+id=myoutput
+cellranger-arc count \
+  --id "$id" \
+  #--fastqs "$fastq_dir" \
+  --libraries "$libraries_csv"\
+  --transcriptome "$par_reference" \
+  --include-introns "$par_include_introns" \
+  "${extra_params[@]}" \
+  --disable-ui \
+
+echo "Copying output"
+if [ -d "$id/outs/" ]; then
+  if [ ! -d "$par_output" ]; then
+    mkdir -p "$par_output"
+  fi
+  mv "$id/outs/"* "$par_output"
+fi
